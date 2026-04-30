8fefb7d0ac24e4a6c4cce2e048d900e7da36f7b9
Sanket-Thorat99 <sanketthorat9935@gmail.com>
2026-04-30 21:49:09 +0530

    Implemented Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <h1 className="text-3xl font-bold text-blue-500">
-      Tailwind Working 🚀
-    </h1>
-    </>
-  )
+  return <Login />;
 }
 
-export default App
+export default App;
